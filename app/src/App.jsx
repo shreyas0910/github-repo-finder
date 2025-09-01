31f5059df428809d8cebc73b826754d0d949b614
shreyas <fortnitekidgamershreyas@gmail.com>
2025-09-01 16:19:59 +0400

    yeah

---
+++ app/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>Github repo finder</h1> 
+      <textarea name="tbox" id="tbox">search for a repository</textarea>
+      <button id='button'>search</button>
+    </>
+  )
+}
+
+export default App
